9a4b48bb856f3b0cd59ef5290b4cddae643a16cf
AmalieKA <a.ka@live.dk>
2024-09-19 16:47:51 +0200

    1

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,53 @@
 <!--
-  1. :style
-  2. find lige ud af hvordan sin/cos bliver regnet ud til at blive x og y + regnestykket med 2pi
-  3. CSS: transform: translate(-50%, -50%); - hvad betyder det??
+   3. CSS: transform: translate(-50%, -50%); - hvad betyder det??
 -->
 
 <script setup>
 import { ref } from 'vue';
 
-const menuIsOpen = ref(false);
-const openOrClose = ref('open');
+const menuIsOpen = ref(false); //set to false upon initialization
+const openOrClose = ref('open'); //the text that is rendered = when the menu closed, it will say "open"
 const buttons = ref(10); // !!! change the number of buttons
-const radius = ref(100);
-const x = ref(0);
-const y = ref(0);
+const radius = ref(100); //the distance from the mouse position to the buttons
+const x = ref(0); //no value yet
+const y = ref(0); //no value yet
 
-function canvasHasBeenClicked(event) {
-  recordMousePosition(event);
+function canvasHasBeenClicked(event) { //when the canvas has been clicked
+  recordMousePosition(event); //executes the function
 
-  if (menuIsOpen.value === false) {
-    menuIsOpen.value = true;
-    openOrClose.value = 'close';
+  if (menuIsOpen.value === false) { //if the menu is closed
+    menuIsOpen.value = true; //the menu opens (see v-show)
+    openOrClose.value = 'close'; //the text is changed to "close"
   } else {
-    menuIsOpen.value = false;
-    openOrClose.value = 'open';
+    menuIsOpen.value = false; //if you click the canvas when the menu is open, the menu closes (see v-show)
+    openOrClose.value = 'open'; //text changes to "open"
   }
 }
 
 function recordMousePosition(event) {
-  x.value = event.pageX;
-  y.value = event.pageY;
+  x.value = event.pageX; //assigns the mouse-x position (event.pageX) as the value to the x variable
+  y.value = event.pageY; //does the same for y
 }
 
-function alertTheUser(number) {
-  alert("You've clicked button number " + number);
-  canvasHasBeenClicked(event);
+function alertTheUser(number) { //when each button is clicked, this function is executed
+  alert("You've clicked button number " + number); //it opens a pop-up stating which button has been clicked (the number of the button) 
+  //canvasHasBeenClicked(event); //keeps the menu open. it changes the center of the menu to the position of the button, so it is disabled
 }
 </script>
 
 <template>
-  <div id="canvas" @click="canvasHasBeenClicked">
+  <!-- when the canvas (id of div) is clicked, the function is executed (event listener) -->
+  <div id="canvas" @click="canvasHasBeenClicked"> 
+    <!-- changes the word in the text to "open" or "close" depending on is the menu is closed or open -->
     <h1 id="clickText">Click anywhere to {{ openOrClose }} the menu!</h1>
+    <!-- for-each: for each button in the button "array" (until number of buttons(see variable "buttons")). index number of each button is called "button" -->
+    <!-- show if menuIsOpen === true -->
+    <!-- if the buttons are clicked, execute funtion alertTheUser using the index-number of the button as an argument in the function -->
+    <!-- :style binds the inline style attribute to the button (special syntax for vue): https://vuejs.org/guide/essentials/class-and-style -->
+    <!-- left uses cos as it (in the unit circle) represents the x-coordinate of a point along the circumference. top: uses sin as it represents y -->
+    <!-- ${x + radius + ...} gives the value of the x-position of the mouse + the radius + ... -->
+    <!-- (2 * Math.PI * button) / buttons) is 2π (circumference of unit circle times the button number (position along the circle) / buttons to divide the circle into x number of positions -->
+    <!-- {{ button }} writes number of button on the button --> 
     <button
       v-for="button in buttons"
       v-show="menuIsOpen"
@@ -52,6 +60,7 @@ function alertTheUser(number) {
     >
       {{ button }}
     </button>
+    <!-- for only 4 buttons: -->
     <!-- <button
       id="button1"
       v-show="menuIsOpen"
